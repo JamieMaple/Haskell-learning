246246f532ef2089a985d4737ece06e1cc4223e1
JamieMaple <jamiemaple007@gmail.com>
2018-08-17 01:06:52 +0800

    feat(hof): add high order function

--- high-order-functions/exer.hs
+++ high-order-functions/exer.hs
@@ -42,3 +42,48 @@ chain n
     | odd n  = n:chain (n*3 + 1)
     | even n = n:chain (n `div` 2)
 
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..1000]))
+    where isLong xs = length xs > 15
+
+sum' :: (Num a) => [a] -> a
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+sum' = foldl (+) 0
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (flip(\acc x -> [f x] ++ acc)) [] xs
+-- map'' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+-- 左折叠往后添加元素效率比较低
+
+elem'' :: (Eq a) => a -> [a] -> Bool
+elem'' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+-- foldable function implements
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+-- maximum
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
+
+-- (&&) :: Bool -> Bool -> Bool
+-- True && x = x
+-- False && _ = False
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
