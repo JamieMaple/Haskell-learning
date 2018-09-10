34841a0876318824cf715a8687dba3b1646feffb
JamieMaple <jamiemaple007@gmail.com>
2018-09-10 13:09:58 +0800

    feat(zippers): add tree zippers

---
+++ zipper/tree.hs
@@ -0,0 +1,19 @@
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving Show
+type Breadcrumbs a = [Crumb a]
+type Zipper a = (Tree a, Breadcrumbs a)
+
+goLeft :: Zipper a -> Maybe (Zipper a)
+goLeft (Node x l r, bs) = Just (l, LeftCrumb x r:bs)
+goLeft (Empty, _) = Nothing
+
+goRight :: Zipper a -> Maybe (Zipper a)
+goRight (Node x l r, bs) = Just (r, RightCrumb x l:bs)
+goRight (Empty, _) = Nothing
+
+goUp :: Zipper a -> Maybe (Zipper a)
+goUp (t, LeftCrumb x r:bs) = Just (Node x t r, bs)
+goUp (t, RightCrumb x l:bs) = Just (Node x l t, bs)
+goUp (_, []) = Nothing
+
+
