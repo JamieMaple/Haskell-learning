32d625204340fca8e4d898a7fea67591ff6627be
Jamie Maple <jamiemaple007@gmail.com>
2019-04-02 23:13:25 +0800

    feat(leetcode): 刷题

---
+++ real-world-haskell/err-handling/ParseInt.hs
@@ -0,0 +1,23 @@
+module ParseInt where
+
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+import Control.Monad.Error
+import Control.Monad.State
+import qualified Data.ByteString.Char8 as B
+
+data ParseError = NumbericOverflow
+                | EndOfInput
+                | Chatty String
+                deriving (Eq, Ord, Show)
+
+instance Error ParseError where
+    noMsg = Chatty "oh noes!"
+    strMsg = Chatty
+
+newtype Parser a = P { 
+    runP :: ErrorT ParseError (State B.ByteString) a
+} deriving (Monad, MonadError ParseError)
+
+
+
