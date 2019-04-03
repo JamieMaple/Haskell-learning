f4b7e2b247df806002a7bb4617efeeae92ed5717
Jamie Maple <jamiemaple007@gmail.com>
2019-04-03 13:16:00 +0800

    feat(leetcode): algorithms

--- 3_longest_substring.go
+++ 3_longest_substring.go
@@ -13,7 +13,7 @@ func allCharUnique(s string) bool {
 	return true
 }
 
-func lengthOfLongestSubstring(s string) int {
+func lengthOfLongestSubstringOn3(s string) int {
 	max := 1
 	for i := 0; i < len(s); i++ {
 		for j := i + 2; j < len(s); j++ {
@@ -22,6 +22,82 @@ func lengthOfLongestSubstring(s string) int {
 				max = offset
 			}
 		}
+	}
+
+	return max
+}
+
+// slide window
+func lengthOfLongestSubstringO2n(s string) int {
+	max := 0
+	i := 0
+	j := 0
+	var set map[byte]bool
+
+	for i < len(s) && j < len(s) {
+		v := s[j]
+		if ok := set[v]; !ok {
+			set[v] = true
+			j++
+			offset := j - i
+			if max < offset {
+				max = offset
+			}
+		} else {
+			v = s[i]
+			set[v] = false
+			i++
+		}
+	}
+
+	return max
+}
+
+// slide window optimized
+func lengthOfLongestSubstringOnOptimized(s string) int {
+	max := 0
+	j := 0
+	m := make(map[byte]int)
+
+	for i := -1; j < len(s); j++ {
+		v := s[j]
+
+		if ii, ok := m[v]; ok && ii > i {
+			i = ii
+		}
+
+		if offset := j - i; offset > max {
+			max = offset
+		}
+
+		m[v] = j
+	}
+
+	return max
+}
+
+func lengthOfLongestSubstring(s string) int {
+	max := 0
+	i := -1
+	m := [128]int{}
+
+	for j := range m {
+		m[j] = -10
+	}
+
+	for j := 0; j < len(s); j++ {
+		index := int(s[j])
+
+		if i < m[index] {
+			i = m[index]
+		}
+
+		offset := j - i
+		if max < offset {
+			max = offset
+		}
+
+		m[index] = j
 	}
 
 	return max
